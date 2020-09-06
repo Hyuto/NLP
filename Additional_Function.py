229e9e7ade4d1e4a2841000d58a7a149e7522bec
Wahyu Setianto <wahyusetianto19@gmail.com>
2020-09-06 12:48:27 +0700

    Update Additional_Function.py

--- Additional_Function.py
+++ Additional_Function.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 import numpy as np
 from tqdm.notebook import tqdm
 
-def ApplyAUG(df, PATH, load_and_preprocess_image, data_augmentation,
-            n_AUG = 1, up_sample_class = None, up_sample_ratio = 0.2):
+def ApplyAUG(df, PATH, LP, data_aug, up_sample_ratio = 0.2,
+            up_sample_class = None):
     """
     Fungsi untuk mengaplikasikan Preprocess dan Augmentation ke dalam data gambar untuk disimpan
     kedalam direktori/file yang baru.
@@ -13,14 +13,25 @@ def ApplyAUG(df, PATH, load_and_preprocess_image, data_augmentation,
     df              : Dataframe yang menyimpan nama file pada kolom 'nama file gambar' dan label pada kolom
                      'label'
     PATH            : Direktori data gambar
-    n_AUG           : Banyaknya jumlah Augmentasi yang akan dilakukan kepada setiap data gambar(Otomatis = 1)
+    LP              : Load & Preprocess image function
+    data_aug        : Augmentation function
+    up_sample_ratio : Rasio up sampling yang dikehendaki.
     up_sample_class : Spesifikasi kelas yang akan dilakukan Augmentasi. Jika ini di isi maka n_AUG
                       akan dihitung secara otomatis tergantung up_sample_ratio.
-    up_sample_ratio : Rasio up sampling yang dikehendaki.
     """
+
+    def __up_sampling(up_sample_ratio, N) -> list:      
+        if up_sample_ratio >= 1:
+            n_AUG = [up_sample_ratio]*N
+        else:
+            n_sample = int(N * up_sample_ratio)
+            n_AUG = [1] * n_sample + [0] * (N - n_sample)
+        return n_AUG
+
     if up_sample_class != None:
-        print(f'[INFO] Up Sampling Kelas {up_sample_class} sebesar {up_sample_ratio * 100}%')
-        print(f'[INFO] n_AUG akan dikalkulasi secara otomatis..\n')
+        print(f'[INFO] Up Sampling Kelas {up_sample_class} Sebesar {up_sample_ratio * 100}%')
+    else:
+        print(f'[INFO] Up Sampling Setiap Kelas Sebesar {up_sample_ratio * 100}%')
     
     DIR = './Prep Data + AUG'
     os.makedirs(DIR)
@@ -31,23 +42,20 @@ def ApplyAUG(df, PATH, load_and_preprocess_image, data_augmentation,
         data = df['nama file gambar'][df.label.values == i].values
         if up_sample_class != None:
             if str(i) == up_sample_class:
-                if up_sample_ratio >= 1:
-                    n_AUG = [up_sample_ratio]*len(data)
-                else:
-                    n_sample = int(len(data) * up_sample_ratio)
-                    n_AUG = [1] * n_sample + [0] * (len(data) - n_sample)
+                n_AUG = __up_sampling(up_sample_ratio, len(data))
             else:
                 n_AUG = [0] * len(data)
         else:
-            n_AUG = [n_AUG]*len(data)
+            n_AUG = __up_sampling(up_sample_ratio, len(data))
         for k, file in enumerate(tqdm(data)):
             IMAGE_DIR = os.path.join(CHILD_DIR, f'{file[:-4]}.png')
-            img = load_and_preprocess_image(PATH + file)
+            img = LP(PATH + file)
             tf.keras.preprocessing.image.save_img(IMAGE_DIR, img)
             for j in range(n_AUG[k]):
                 AUG_DIR = os.path.join(CHILD_DIR, f'AUG {j + 1}_{file[:-4]}.png')
-                aug = data_augmentation(np.expand_dims(img, 0))
+                aug = data_aug(np.expand_dims(img, 0))
                 tf.keras.preprocessing.image.save_img(AUG_DIR, aug[0])
         print(f'[INFO] Selesai Memproses Kelas {i}')
         print('[INFO] ' + f'Banyak Data Kelas {i} setelah proses sebanyak {len(os.listdir(CHILD_DIR))} gambar\n'.title())
+    print(f'[INFO] Saved to {DIR}')
     print('[INFO] Done :)')
